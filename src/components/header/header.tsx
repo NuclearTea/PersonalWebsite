b4959aed38ac6ab84ee1228b2c4bafa99984a067
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-08-23 11:08:30 -0600

    on name hover handlers

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,25 @@
+import { useCallback, useState } from 'react';
 import styles from './header.module.scss';
 
 export const Header = () => {
+  const [hoverName, setHoverName] = useState(false);
+
+  const handleMouseOver = useCallback(() => setHoverName(true), []);
+  const handleMouseLeave = useCallback(() => setHoverName(false), []);
   return (
-    <div className={styles.header}>
-      <h4 className={styles.start}>Muhammad Ali</h4>
-      <h4 className={styles.end}>Another 1</h4>
-      <h4 className={styles.end}>Another 2</h4>
+    <div>
+      <div className={styles.header}>
+        <h4
+          onMouseEnter={handleMouseOver}
+          onMouseLeave={handleMouseLeave}
+          className={styles.start}
+        >
+          Muhammad Ali
+        </h4>
+        <h4 className={styles.end}>Another 1</h4>
+        <h4 className={styles.end}>Another 2</h4>
+      </div>
+      {hoverName && <h4>Another 3</h4>}
     </div>
   );
 };
