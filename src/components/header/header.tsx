c3b801a37c09febe1555fe7a648ff148c543ec16
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-08-22 22:57:47 -0600

    Getting started, creating initial header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import './header.css';
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <h4>Muhammad Ali</h4>
+      <div className="end">
+        <h4>alskjf</h4>
+      </div>
+      {/* <h4>{import.meta.env.VITE_VAR}</h4> */}
+    </div>
+  );
+};
