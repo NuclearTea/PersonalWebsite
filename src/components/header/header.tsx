2a534e94494b933f997d8169d1ee38a6b0c358bb
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-08-24 08:05:33 -0600

    Clean up header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,11 @@
 import styles from './header.module.scss';
-import { Tooltip } from '../tooltip/tooltip';
 import { Links } from './links/links';
 
 export const Header = () => {
   return (
-    <div>
-      <div className={styles.header}>
-        <div className={styles.start}>
-          {/* <Tooltip title={<h4>Muhammad Ali</h4>} content={<Links />} /> */}
-          <h4>Muhammad Ali</h4>
-        </div>
-        <Links />
-        {/* <h4 className={styles.end}>Another 1</h4>
-        <h4 className={styles.end}>Another 2</h4> */}
-      </div>
+    <div className={styles.header}>
+      <h4 className={styles.start}>Muhammad Ali</h4>
+      <Links />
     </div>
   );
 };
