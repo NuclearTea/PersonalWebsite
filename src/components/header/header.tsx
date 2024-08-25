8889b52f759cdffb657e8c10547f36247f9b2455
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-08-25 12:30:07 -0600

    clean up header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,9 @@
 import styles from './header.module.scss';
-import { Links } from './links/links';
 
 export const Header = () => {
   return (
     <div className={styles.header}>
       <h4 className={styles.start}>Muhammad Ali</h4>
-      {/* <Links /> */}
     </div>
   );
 };
