59922550ef275eee1261322b842b5dcecf38f7b1
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-08-24 18:39:00 -0600

    add intro pic and display in component

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,3 +1,10 @@
+import intro_pic from '../../assets/Intro_Picture.jpg';
+import styles from './intro.module.scss';
 export const Intro = () => {
-  return <div>This is the Intro</div>;
+  return (
+    <div className={styles.intro}>
+      <img src={intro_pic} className={styles.intro_pic} />
+      <h2>Muhammad Ali</h2>
+    </div>
+  );
 };
