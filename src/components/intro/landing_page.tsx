f018cebb18daf496ff89385cfa37d2ce7ca68f2d
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-08-24 19:55:19 -0600

    update landing page scroll

--- src/components/intro/landing_page.tsx
+++ src/components/intro/landing_page.tsx
@@ -3,8 +3,9 @@ import { IntroCard } from './intro_card';
 
 export const Intro = () => {
   return (
-    <div className={styles.intro}>
+    <div className={`${styles.intro} ${styles.example}`}>
       <IntroCard />
+      <div style={{ height: '25rem' }}>a;slkjfas</div>
     </div>
   );
 };
