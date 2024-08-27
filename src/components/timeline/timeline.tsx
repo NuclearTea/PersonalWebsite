125cd87ef70d483553072ab8fcd387a706a193ac
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-08-27 11:02:17 -0600

    create function to display cards

--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -1,24 +1,43 @@
+import { useCallback } from 'react';
 import { Card } from '../card/card';
 import { Circle } from '../circle/circle';
 import styles from './timeline.module.scss';
 import { TimelineProps } from './timeline.types';
 
 export const Timeline = ({ items }: TimelineProps) => {
-  // const evenItems= items.filter((_item, i) => i % 2 === 0);
-  // const oddItems = items.filter((_item, i) => i % 2 === 1);
+  const displayCards = useCallback(
+    (evenOrOdd: 'even' | 'odd') => {
+      const height = '20rem';
+      const width = '20rem';
+      return (
+        <div className={styles.timeline_items}>
+          {items.map((item, i) => {
+            const card = (
+              <Card
+                key={`${evenOrOdd}-${item.title}`}
+                height={height}
+                width={width}
+                title={item.title}
+                content={item.content}
+                border="none"
+                titleSeparator="none"
+              />
+            );
+            if (i % 2 === (evenOrOdd === 'even' ? 0 : 1)) {
+              return card;
+            }
+            return <div style={{ height, width }} />;
+          })}
+        </div>
+      );
+    },
+    [items]
+  );
   return (
     <span className={styles.timeline}>
+      {displayCards('even')}
       <div className={styles.timeline_items}>
-        {items.map((item, i) => {
-          const card = <Card height="5rem" width="20rem" title={item.title} />;
-          if (i % 2 === 0) {
-            return card;
-          }
-          return <div style={{ height: '5rem', width: '20rem' }} />;
-        })}
-      </div>
-      <div className={`${styles.timeline_items} `}>
-        {Array.from({ length: items.length }, () => (
+        {Array.from({ length: items.length }, (_, i) => (
           <div
             style={{
               display: 'flex',
@@ -27,20 +46,12 @@ export const Timeline = ({ items }: TimelineProps) => {
               alignItems: 'center',
             }}
           >
-            <Circle height="8rem" width="8rem" />
+            <Circle height="8rem" width="8rem" content={items[i].date} />
             <div className={styles.timeline_connector} />
           </div>
         ))}
       </div>
-      <div className={styles.timeline_items}>
-        {items.map((item, i) => {
-          const card = <Card height="5rem" width="20rem" title={item.title} />;
-          if (i % 2 === 1) {
-            return card;
-          }
-          return <div style={{ height: '5rem', width: '20rem' }} />;
-        })}
-      </div>
+      {displayCards('odd')}
     </span>
   );
 };
--- src/components/timeline/timeline.types.ts
+++ src/components/timeline/timeline.types.ts
@@ -1,6 +1,7 @@
 export type TimelineItem = {
   title: string | React.ReactNode;
   content: string | React.ReactNode;
+  date?: string;
 };
 export type TimelineProps = {
   items: TimelineItem[];
