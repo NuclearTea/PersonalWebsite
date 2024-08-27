2a24a7473c833b608c8f1317a027b18c62fec042
Muhammad Ali <87144559+NuclearTea@users.noreply.github.com>
2024-08-27 11:00:40 -0600

    add items to timeline

--- src/components/intro/landing_page.tsx
+++ src/components/intro/landing_page.tsx
@@ -13,13 +13,55 @@ export const LandingPage = () => {
         content={
           <Timeline
             items={[
-              { content: 'hello', title: 'hello' },
-              { content: 'something else', title: 'you' },
+              {
+                title: <div style={{ marginBottom: '0' }}>UDEAL</div>,
+                content: (
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'flex-start',
+                    }}
+                    role="button"
+                    tabIndex={0}
+                  >
+                    <a
+                      href="https://github.com/NuclearTea/UDeal"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{
+                        textDecoration: 'none',
+                        color: 'black',
+                        margin: '1rem',
+                        marginTop: '0rem',
+                      }}
+                    >
+                      <p>
+                        The beginning of my full stack development journey
+                        starts with a university project for my Database
+                        Management Systems class. Udeal was a final project
+                        which aimed to be a university student-only marketplace.
+                        Users would sign up using their university credentials
+                        (to verify their student status) and be able to post,
+                        edit, and delete listings.
+                      </p>
+                      <h4>Click for more details</h4>
+                    </a>
+                  </div>
+                ),
+                date: 'April 2022',
+              },
+              { content: 'something else1', title: 'you1' },
+              { content: 'something else2', title: 'you2' },
+              { content: 'something else3', title: 'you3' },
+              { content: 'something else4', title: 'you4' },
             ]}
           />
         }
         width="100%"
         height="fit-content"
+        border="none"
+        borderRadius="none"
       />
     </div>
   );
